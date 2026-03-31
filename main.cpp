9b671a95e9c5a44909293de0ab1541749387c606
justinguto <jutogustowski@gmail.com>
2026-03-31 13:30:37 -0700

    Complete a58 file write and read program

--- main.cpp
+++ main.cpp
@@ -11,32 +11,53 @@ int main()
     string stuName;
     ofstream ofs;
 
-    // TODO: open "students.txt" for writing
+    ofs.open("students.txt");
+
+    if (!ofs)
+    {
+        cout << "Error opening file" << endl;
+        exit(0);
+    }
 
     cout << "Enter the total number of students:";
-    // TODO: read N from cin, write N to ofs
+    cin >> N;
+    ofs << N << endl;
 
     for (int i = 0; i < N; i++)
     {
         cout << "Enter the student name and two scores: ";
-        // TODO: read stuName, score1, score2 from cin; write them to ofs
+        cin >> stuName >> score1 >> score2;
+        ofs << stuName << " " << score1 << " " << score2 << endl;
     }
-    // TODO: close ofs
+    ofs.close();
 
     // === Part 2: Read student data from students.txt and print stats ===
     int M;
     double sum, avg;
     ifstream ifs;
 
-    // TODO: open "students.txt" for reading
+    ifs.open("students.txt");
 
-    // TODO: read M (total students) from ifs, print "Total M students"
+    if (!ifs)
+    {
+        cout << "Error opening file" << endl;
+        exit(0);
+    }
+
+    ifs >> M;
+    cout << "Total " << M << " students" << endl;
 
     for (int i = 0; i < M; i++)
     {
-        // TODO: read stuName, score1, score2 from ifs
-        // TODO: compute sum = score1 + score2, avg = sum / 2.0
-        // TODO: print "Student Name: <name>  score1: <s1>  score2: <s2>  Sum: <sum>  Avg: <avg>"
+        ifs >> stuName >> score1 >> score2;
+        sum = score1 + score2;
+        avg = sum / 2.0;
+
+        cout << "Student Name: " << stuName
+             << " score1: " << score1
+             << " score2: " << score2
+             << " Sum: " << sum
+             << " Avg: " << avg << endl;
     }
     ifs.close();
 
